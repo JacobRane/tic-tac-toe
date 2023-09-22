9360e459192f447aec91f9f2cd3b4233d3481694
Jacob Rane <135966107+JacobRane@users.noreply.github.com>
2023-09-22 21:48:00 +0200

    Unsure of when last save was

--- script.js
+++ script.js
@@ -6,22 +6,40 @@
 // When 3 squares in a row are filled by the same type of piece a winner will be declared
 // A button to reset the game will show up.
 
-
-let gameBoard = {
-    gameBoardArray: ['','','','','','','','',''],
+// Gameboard stuff
+const gameBoard = {
+    gameBoardArray: ['','','','','','','','','','','',''],
 }
 
+const boardSquare = document.querySelector('.boardSquare');
 let gameBoardDisplay = (array) => {
-    let boardSquare = document.querySelector('.boardSquare');
     for (let i = 0; i < array.length; i++) {
         const boardSquareDiv = document.createElement('div')
         boardSquareDiv.classList.add(`square${i}`)
         boardSquareDiv.textContent = array[i]
         boardSquare.appendChild(boardSquareDiv)
+        boardSquareDiv.addEventListener('click', () => {
+            if (gameBoard.gameBoardArray[i] === '') {
+                gameBoard.gameBoardArray[i] = currentPiece
+            }
+            clearTheGameBoard(boardSquare) 
+            gameBoardDisplay(gameBoard.gameBoardArray)
+            document.querySelector('.buttonsDiv').style.display = 'none'
+            if (currentPiece === 'x') {currentPiece = 'o'}
+            else if (currentPiece === 'o') {currentPiece = 'x'}
+        })
+    } 
+}
+
+let clearTheGameBoard = (parent) => {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
 }
+
 gameBoardDisplay(gameBoard.gameBoardArray)
 
+// Choose piece
 let currentPiece;
 const choosePieceButtonX = document.querySelector('.choosePieceButtonX')
 const choosePieceButtonO = document.querySelector('.choosePieceButtonO')
@@ -31,4 +49,3 @@ choosePieceButtonO.addEventListener('click', () => {
 choosePieceButtonX.addEventListener('click', () => {
     currentPiece = 'x'
 })
-
